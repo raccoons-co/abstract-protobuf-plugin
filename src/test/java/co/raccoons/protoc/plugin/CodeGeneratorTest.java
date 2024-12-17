c2c4bd10e793d7bf6dec02b9e58982828bc7cac8
iselo <50623030+iselo@users.noreply.github.com>
2024-12-17 21:04:02 +0200

    Test `CodeGenerator`

--- src/test/java/co/raccoons/protoc/plugin/CodeGeneratorTest.java
+++ src/test/java/co/raccoons/protoc/plugin/CodeGeneratorTest.java
@@ -6,15 +6,37 @@
 
 package co.raccoons.protoc.plugin;
 
+import com.google.common.testing.NullPointerTester;
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.File;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("CodeGenerator")
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Multipart Code Generator")
 class CodeGeneratorTest {
 
     @Test
-    void nothing() {
-        //fails();
+    @DisplayName("not accepts `null`")
+    void throwsExceptionOnNull(){
+        var generator = CodeGenerator.newBuilder().build();
+        new NullPointerTester().testAllPublicInstanceMethods(generator);
     }
 
+    @Test
+    @DisplayName("adds concrete code generator")
+    void addsCodeGenerator() {
+        var generator = CodeGenerator.newBuilder()
+                .add(generatorMock)
+                .build();
+        assertEquals(1, generator.generators().size());
+    }
+
+    private final AbstractCodeGenerator generatorMock =
+            new AbstractCodeGenerator() {
+                @Override
+                protected File generate(ProtocolType type) {
+                    return File.getDefaultInstance();
+                }
+            };
 }
